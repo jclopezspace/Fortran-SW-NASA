dfef11f7c88b909485e074226c3b68d3d38f75c2
Steve Winward <2002602+SteveWinward@users.noreply.github.com>
2025-05-26 17:26:35 +0000

    adding ASCI art to the dotnet solution

--- dotnetSrc/ModernFortran/Program.cs
+++ dotnetSrc/ModernFortran/Program.cs
@@ -6,6 +6,44 @@ Console.WriteLine("James Madison University, Mathematics Department");
 Console.WriteLine("_________________________________________________________");
 Console.WriteLine();
 
+# region ACSI ART :)
+
+Console.WriteLine("       _    ");
+Console.WriteLine("      /_\\  ");
+Console.WriteLine("     /___\\ ");
+Console.WriteLine("    |=   =| ");
+Console.WriteLine("    | NASA| ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("    |     | ");
+Console.WriteLine("   /|##!##|\\");
+Console.WriteLine("  / |##!##| \\");
+Console.WriteLine(" /  |##!##|  \\");
+Console.WriteLine("|  / ^ | ^ \\  |");
+Console.WriteLine("| /  ( | )  \\ |");
+Console.WriteLine("|/   ( | )   \\|");
+Console.WriteLine("    ((   ))");
+Console.WriteLine("   ((  :  ))");
+Console.WriteLine("   ((  :  ))");
+Console.WriteLine("    ((   ))");
+Console.WriteLine("     (( ))");
+Console.WriteLine("      ( )");
+Console.WriteLine("       .");
+Console.WriteLine("       .");
+Console.WriteLine("       .");
+Console.WriteLine();
+
+# endregion
+
 string filename = "FILTER_IN.DAT";
 
 // Read in the filter data
@@ -56,11 +94,79 @@ int[] iterations = new int[99];
 
 Console.WriteLine("(W, Iterations)");
 
+# region Scatter Plot of W vs the number of iterations
+
 for (int i = 1; i <= 99; i++)
 {
     double w = 1.0 + i * 0.01;
     iterations[i - 1] = FortranModule.SuccOverRelaxation(lVector, dVector, uVector, bVector, xInitial, w, nMinus2);
     for (int j = 0; j < xInitial.Length; j++) xInitial[j] = 0.0;
 
-    Console.WriteLine($"{w:F2}, {iterations[i-1]}");
+    Console.WriteLine($"{w:F2}, {iterations[i - 1]}");
 }
+
+int maxIterations = 0;
+for (int i = 0; i < iterations.Length; i++)
+{
+    if (iterations[i] > maxIterations)
+        maxIterations = iterations[i];
+}
+
+int plotHeight = 20; // Number of rows for the y-axis
+int plotWidth = iterations.Length; // One column per w value
+
+// Scale function for y-axis
+int ScaleY(int value)
+{
+    return plotHeight - 1 - (int)Math.Round((double)value / maxIterations * (plotHeight - 1));
+}
+
+// Build plot
+char[,] plot = new char[plotHeight, plotWidth];
+for (int y = 0; y < plotHeight; y++)
+    for (int x = 0; x < plotWidth; x++)
+        plot[y, x] = ' ';
+
+// Plot points
+for (int x = 0; x < plotWidth; x++)
+{
+    int y = ScaleY(iterations[x]);
+    plot[y, x] = '*';
+}
+
+// Print y-axis labels and plot
+Console.WriteLine();
+for (int y = 0; y < plotHeight; y++)
+{
+    int iterLabel = (int)Math.Round(maxIterations * (double)(plotHeight - 1 - y) / (plotHeight - 1));
+    Console.Write($"{iterLabel,4} | ");
+    for (int x = 0; x < plotWidth; x++)
+        Console.Write(plot[y, x]);
+    Console.WriteLine();
+}
+
+// Print x-axis
+Console.Write("     +");
+for (int x = 0; x < plotWidth; x++)
+    Console.Write('-');
+Console.WriteLine();
+
+// Print x-axis labels (w values at start, middle, end)
+double wStart = 1.01;
+double wEnd = 2.0;
+int mid = plotWidth / 2;
+Console.Write("      ");
+for (int x = 0; x < plotWidth; x++)
+{
+    if (x == 0)
+        Console.Write($"{wStart:F2}");
+    else if (x == mid)
+        Console.Write($"{wStart + (wEnd - wStart) / 2:F2}".PadLeft(mid - 2));
+    else if (x == plotWidth - 1)
+        Console.Write($"{wEnd:F2}".PadLeft(plotWidth - mid - 4));
+    else
+        Console.Write(" ");
+}
+Console.WriteLine();
+
+# endregion
